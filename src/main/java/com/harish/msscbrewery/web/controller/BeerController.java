31fafa46f6833833da8407ab4ff5c229cff25aa3
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-11-09 13:55:58 +0530

    Update Beer

--- src/main/java/com/harish/msscbrewery/web/controller/BeerController.java
+++ src/main/java/com/harish/msscbrewery/web/controller/BeerController.java
@@ -33,4 +33,11 @@ public class BeerController {
 
         return new ResponseEntity(headers, HttpStatus.CREATED);
     }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity handleUpdate(@PathVariable UUID beerId, BeerDto beerDto) {
+        beerService.updateBeer(beerId, beerDto);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
 }
