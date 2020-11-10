b04c06637a65942695a88ee6c1a898c90e6cee84
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-11-10 11:11:56 +0530

    Delete Beer method

--- src/main/java/com/harish/msscbrewery/web/services/BeerServiceImpl.java
+++ src/main/java/com/harish/msscbrewery/web/services/BeerServiceImpl.java
@@ -1,11 +1,13 @@
 package com.harish.msscbrewery.web.services;
 
 import com.harish.msscbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class BeerServiceImpl implements BeerService {
     @Override
     public BeerDto getBeerById(UUID beerId) {
@@ -25,5 +27,10 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public void updateBeer(UUID beerId, BeerDto beerDto) {
 
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting a beer...");
     }
 }
