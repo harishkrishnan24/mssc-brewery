fafaf7c98441cd48a94abff123937cc830c94714
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-11-14 12:56:40 +0530

    Add mapper for Beer

---
+++ src/main/java/com/harish/msscbrewery/web/mappers/BeerMapper.java
@@ -0,0 +1,13 @@
+package com.harish.msscbrewery.web.mappers;
+
+import com.harish.msscbrewery.domain.Beer;
+import com.harish.msscbrewery.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+
+    BeerDto beerToBeerDto(Beer beer);
+
+    Beer beerDtoToBeer(BeerDto beerDto);
+}
